a9ed9629aa51673441c94726ea494915edfac9a7
Alok Pal <smp.alok84@gmail.com>
2021-10-16 16:27:17 +0530

    spring jwt example learning and committed

---
+++ JWT-Demo/src/main/java/com/dailycoder/jwt/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.dailycoder.jwt.controller;
+
+import com.dailycoder.jwt.utility.JWTUtility;
+import com.dailycoder.jwt.model.JwtRequest;
+import com.dailycoder.jwt.model.JwtResponse;
+import com.dailycoder.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String getString(){
+        return "Welcome to DailyCoder!";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return  new JwtResponse(token);
+    }
+}
